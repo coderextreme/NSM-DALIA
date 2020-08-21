a383bfafc4cd2ba5746aec0ecfa428cdf6626d25
John Carlson <yottzumm@gmail.com>
2020-08-21 16:49:59 -0500

    did a git diff -w

--- john/testscript.sh
+++ john/testscript.sh
@@ -1,5 +1,5 @@
-cd ../nsm-dalia-0.9
 OUTDIR=`pwd`
+cd ../nsm-dalia-0.9
 mkdir -p $OUTDIR
 /c/Program\ Files/swipl/bin/swipl.exe > $OUTDIR/parselog.txt<<EOF
 [dalia_cline].
@@ -9,3 +9,4 @@ halt.
 EOF
 grep noparse $OUTDIR/parselog.txt > $OUTDIR/noparse.txt
 cd $OUTDIR
+git diff -w .

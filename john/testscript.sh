7aa127fddd0018a01a2a3a5174e8b31bc7aca00f
John Carlson <yottzumm@gmail.com>
2020-08-21 17:25:12 -0500

    changed to C: because /c may not exist

--- john/testscript.sh
+++ john/testscript.sh
@@ -2,7 +2,7 @@ DALIADIR=../nsm-dalia-0.9
 OUTDIR=`pwd`
 cd $DALIADIR
 mkdir -p $OUTDIR
-/c/Program\ Files/swipl/bin/swipl.exe > $OUTDIR/parselog.txt<<EOF
+C:/Program\ Files/swipl/bin/swipl.exe > $OUTDIR/parselog.txt<<EOF
 [dalia_cline].
 l(eng).
 pf("../john/EnglishNSM.txt").

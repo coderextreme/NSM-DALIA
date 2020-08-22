b41ebd9ea06f1509f7aa8c7ca2cebdb3f8b8a09e
John Carlson <yottzumm@gmail.com>
2020-08-22 16:42:25 -0500

    renamed john to test-nsm-dalia

--- test-nsm-dalia/testscript.sh
+++ test-nsm-dalia/testscript.sh
@@ -5,7 +5,7 @@ mkdir -p $OUTDIR
 C:/Program\ Files/swipl/bin/swipl.exe > $OUTDIR/parselog.txt<<EOF
 [dalia_cline].
 l(eng).
-pf("../john/EnglishNSM.txt").
+pf("../test-nsm-dalia/EnglishNSM.txt").
 halt.
 EOF
 grep noparse $OUTDIR/parselog.txt > $OUTDIR/noparse.txt
